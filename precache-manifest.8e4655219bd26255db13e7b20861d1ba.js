c140de5123e776f862bb2a98d0978963bfa048b0
Olik1992 <ovlyakul30041992@gmail.com>
2020-02-08 13:00:13 +0200

    react app files

---
+++ precache-manifest.8e4655219bd26255db13e7b20861d1ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79250901943aec162aa2efc5f7a8f92f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d79819933dcb7d3a8362",
+    "url": "/static/css/main.c0c14cb7.chunk.css"
+  },
+  {
+    "revision": "b829692f51f58c98b1bd",
+    "url": "/static/js/2.d29b3332.chunk.js"
+  },
+  {
+    "revision": "d79819933dcb7d3a8362",
+    "url": "/static/js/main.cfafc2b7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
